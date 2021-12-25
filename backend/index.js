0b21875c22d4803d1843e78f13ffcbdf2b40e901
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-12-25 21:11:51 +0530

    Added user and notes schema && Added user and notes routes

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
     res.send('Radhey Radhey')
   })
 
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
+
 app.listen(port, () => {
-    console.log(`Success: http://localhost:${port}`)
+    console.log(`Serving On: http://localhost:${port}`)
   })
---
+++ backend/models/Notes.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+  title:{type:String , required:true},
+  description:{type:String , required:true , unique:true},
+  tag:{type:String , default:"General"},
+  date:{type:String , default:Date.now},
+});
+
+module.exports = mongoose.model('note', NotesSchema);
