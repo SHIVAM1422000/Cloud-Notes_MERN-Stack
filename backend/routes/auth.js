515c4424a128850e398a30cdcd8f5bb6d5302d25
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-12-27 12:08:17 +0530

    Added User Verification After Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const JWT_Secret="RadhaBallabhSriHarivansh``````";
 
 
 // ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
+// ============================================================================================
 router.post('/create_user',[
     body('name',"wrong").isLength({ min: 3 }),
     body('email',"wrong").isEmail(),
@@ -20,8 +21,8 @@ router.post('/create_user',[
 
     try{
         // Check whether the user with this email exists already     
-      let newUser=await User.findOne({email:req.body.email});
-      if(newUser){
+      let user=await User.findOne({email:req.body.email});
+      if(user){
         return res.status(400).json({ error: "Sorry a user with this email already exists" });
       }
       
@@ -30,7 +31,7 @@ router.post('/create_user',[
       const secPass=await bcrypt.hash(req.body.password,salt);
 
    // Create a new user
-     newUser=await User.create({
+     user=await User.create({
       name: req.body.name,
       email: req.body.email,
       password: secPass,
@@ -38,12 +39,11 @@ router.post('/create_user',[
 
      //json web token
      var jwt = require('jsonwebtoken');
-     var token = jwt.sign({ id: newUser.id }, JWT_Secret);
-     console.log("User Id: "+ newUser.id);
+     var token = jwt.sign({ id: user.id }, JWT_Secret);
      console.log(token);
 
 
-    res.json(newUser);
+    res.json(user);
 
   }catch(error){
  
@@ -53,9 +53,53 @@ router.post('/create_user',[
   }
 
 
-    console.log(req.body); 
-    // res.send(req.body); 
 });
+
+
+
+
+
+// ROUTE 2: Create a User using: POST "/api/auth/login". 
+// ===================================================================
+router.post('/login',[
+  body('email', 'Enter a valid email').isEmail(), 
+  body('password', 'Password cannot be blank').exists(), 
+],async (req,res)=>{
+ // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: "Please try to login with correct credentials" });
+  }
+
+  try{
+      // Check whether the user with this email exists or not     
+    let user=await User.findOne({email:req.body.email});
+    if(!user){
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
+    }
+
+
+    //Password verification
+    const passwordCompare = await bcrypt.compare(req.body.password, user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error: "Wrong Credentials"});
+    }
+
+   //json web token
+   var jwt = require('jsonwebtoken');
+   var token = jwt.sign({ id: user.id }, JWT_Secret);
+   console.log(token);
+   res.json(token);
+
+}catch(error){
+
+  console.log(error.message);
+  res.status(500).send("Internal Server Error");
+
+}
+
+});
+
 
 
 
