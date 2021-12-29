cd0ffad0e004acaade23452ed551cd573d257bc0
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-12-29 10:29:30 +0530

    Started Working On Fronted and Added Bootstrap , Navbar and Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-       <h1>Mansaarovar</h1>
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar/>
+       <h1 className='text-warning'>Mansaarovar</h1>
+    
+        {/* Navigations Links  */}
+    {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav> */}
+        
+
+        {/* // adiing paths here */}
+        <Switch>
+          <Route exact path="/"><Home/></Route>
+          <Route exact path="/about"> <About/></Route>
+        </Switch>
+
+</div>
+    </Router>
+    
+
   );
 }
 
